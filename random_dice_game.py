2bfb428b9a0879d7f90eb9c4c5ab92a99c46a9fd
FuzeHere <fairuzofficial7@gmail.com>
2025-06-04 10:56:05 +0800

    Add random_dice_game.py

---
+++ random_dice_game.py
@@ -0,0 +1,13 @@
+import random
+
+while True:
+    ask = input("Do you want roll the dice? (y/n): ").lower()
+    if ask == "y":
+        roll_1 = random.randint(1, 6)
+        roll_2 = random.randint(1, 6)
+        print(f"{roll_1}, {roll_2}")
+    elif ask == "n":
+        print("Okay see u next game ^^")
+        break
+    else:
+        print("Invalid Choice, Try Again")
