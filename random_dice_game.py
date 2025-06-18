47167138c1ab6d5e7f11d131873f0dfdc5d8ee08
Fairuz Istighfar <fairuzofficial7@gmail.com>
2025-06-18 10:33:51 +0800

    Update random_dice_game.py

--- random_dice_game.py
+++ random_dice_game.py
@@ -6,7 +6,7 @@ while True:
         roll_1 = random.randint(1, 6)
         roll_2 = random.randint(1, 6)
         total = r1 + r2
-        print(f"{roll_1}, {roll_2}")
+        print(f"The dice shows ({roll_1}, {roll_2})")
     elif ask == "n":
         print("Okay see u next game ^^")
         break
